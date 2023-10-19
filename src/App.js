3632e362f68abd3efd85dcb9cf5284719bc3c037
Pratinav Shrivastava <pratinavshrivastava6@gmail.com>
2023-10-19 18:34:14 +0530

    Images fetched dynamically from TMDB using API and added to website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import React from 'react';
 import './App.css';
+import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
     <h1>Hey Pratinav lets build Netflix Clone today </h1>
+    <Row title="NETFLIX ORIGINALS" fetchURL={requests.fetchNetflixOriginals} />
+    <Row title="Trending Now" fetchURL={requests.fetchTrending} />
     </div>
   );
 }
