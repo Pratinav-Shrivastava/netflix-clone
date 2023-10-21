a8fb6e03717144db5277f7ed5bfe96189d47136c
Pratinav Shrivastava <pratinavshrivastava6@gmail.com>
2023-10-22 01:04:56 +0530

    Banner almost completed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   return (
     <div className="app">
       {/* Navbar */}
-      {/* Banner */}
       <Banner/>
       <Row title="NETFLIX ORIGINALS" 
         fetchURL={requests.fetchNetflixOriginals}
